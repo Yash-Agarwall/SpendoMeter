7e3727c056ec3fc1bdf236540e2eb929dd11dcc3
Yash Agarwal <yashagarwal7088@gmail.com>
2025-10-09 20:54:45 +0530

    Improved UI

--- src/TransactionList.jsx
+++ src/TransactionList.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import "./TransactionList.css";
 
 export default function TransactionList({ transactions, onDelete, onEdit }) {
-  if (transactions.length == 0) {
-    return <p>No Transaction yet</p>;
+  if (transactions.length === 0) {
+    return <p className="no-transactions">No Transactions yet</p>;
   }
   return (
-    <>
-      <h4>Transactions</h4>
+    <div className="transaction-list">
+      <h3>Transactions</h3>
       <ul>
         {transactions.map((transaction) => (
           <TransactionItem
@@ -17,43 +18,65 @@ export default function TransactionList({ transactions, onDelete, onEdit }) {
           />
         ))}
       </ul>
-    </>
+    </div>
   );
 }
 
 function TransactionItem({ transaction, onDelete, onEdit }) {
-  let [showDetails, setShowDetails] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
 
   return (
-    <li onClick={() => setShowDetails((prev) => !prev)}>
-      <div>
-        <span>{transaction.name}</span>
-        <span>₹{transaction.amount}</span>
-        <button
-          onClick={(e) => {
-            e.stopPropagation();//to avoid opening of the details upon clicking
-            onDelete(transaction.id);
-          }}
-        >
-          <span className="material-symbols-outlined">delete</span>
-        </button>
-        <button onClick={(e)=>{  
-            e.stopPropagation();                     
-            onEdit(transaction)
-        }}><span className="material-symbols-outlined">edit</span></button>
+    <li
+      className="transaction-item"
+      onClick={() => setShowDetails((prev) => !prev)}
+    >
+      <div className="transaction-summary">
+        <div className="transaction-main">
+          <span className="transaction-name">
+            {transaction.name} — <span>{transaction.category}</span>
+          </span>
+          <span className="transaction-amount">₹{transaction.amount}</span>
+        </div>
+        <div className="transaction-actions">
+          <button
+            // className="icon-btn delete-btn"
+            onClick={(e) => {
+              e.stopPropagation();
+              onDelete(transaction.id);
+            }}
+          >
+            <span className="material-symbols-outlined">delete</span>
+          </button>
+          <button
+            // className="icon-btn edit-btn"
+            onClick={(e) => {
+              e.stopPropagation();
+              onEdit(transaction);
+            }}
+          >
+            <span className="material-symbols-outlined">edit</span>
+          </button>
+        </div>
       </div>
+
       {showDetails && (
-        <div>
+        <div className="transaction-details">
           <p>
             <b>Date:</b> {transaction.date}
           </p>
           <p>
-            <b>Category: </b>
-            {transaction.type}
+            <b>Type: </b>
+            <span
+              className={`transaction-type ${transaction.type.toLowerCase()}`}
+            >
+              {transaction.type}
+            </span>
           </p>
           <p>
-            <b>Description: </b>
-            {transaction.description}
+            <b>Description:</b> {transaction.description}
+          </p>
+          <p>
+            <b>Category:</b> {transaction.category}
           </p>
         </div>
       )}
